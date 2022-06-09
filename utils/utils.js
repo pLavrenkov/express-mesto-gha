72906b658dc3bbc3d022a7fdaaad7b25b632bee3
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-06-09 12:56:07 +0300

    PW13 hotfix v2_0

--- utils/utils.js
+++ utils/utils.js
@@ -1,14 +1,14 @@
 module.exports.handleError = (id, err, req, res) => {
-  if (id in req.params && `req.params.${id}.length` !== 24) {
+  /* if (id in req.params && `req.params.${id}.length` !== 24) {
     res.status(400).send({ message: `Введен некорректный ID объекта: ${err.message}` });
     return;
-  }
+  } */
   if (err.name === 'ValidationError') {
     res.status(400).send({ message: `Введены некорректные данные: ${err.message}` });
     return;
   }
   if (err.name === 'CastError') {
-    res.status(404).send({ message: `Данные не обнаружены: ${err.message}` });
+    res.status(400).send({ message: `Введены некорректные данные: ${err.message}` });
     return;
   }
   res.status(500).send({ message: `Произошла ошибка: ${err.message}` });
